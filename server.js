27211fb8b6fdbd7be4e53f41d9e2846f8117c3a5
lrush85 <lance.rush1@gmail.com>
2019-02-20 20:51:12 -0600

    Updated js file

--- server.js
+++ server.js
@@ -21,19 +21,69 @@ app.get("/view", function (req, res) {
    res.sendFile(path.join(__dirname, "view.html"));
 });
 
-//Displays api reservations
+//Displays api reservation tables
 app.get("/api/tables", function (req, res) {
-
+    return res.json(tables);
 });
 
+// Displays api waitlist
 app.get("/api/waitlist", function (req, res) {
-
+    return res.json(waitlist);
 });
 
 
+// Reservation Tables (DATA)
+//==================================================
+var tables = [
+    {
+      name: "lance",
+      phoneNumber: 3,
+      email: "lance.rush1@gmail.com",
+      uniqueId: "F33GDL",
+    },
+    {
+      name: "jay",
+      phoneNumber: 44,
+      email: "jaysgottoloveit@gmail.com",
+      uniqueId: "F33SSDL",
+    }
+  ];
+
+
+  // Waitlist (DATA)
+  var waitlist = [
+    {
+        name: "lance",
+        phoneNumber: 3,
+        email: "lance.rush1@gmail.com",
+        uniqueId: "F33GDL",
+      },
+  ];
 
 
 
+  // Create new reservation
+  app.post("/api/tables", function(req, res) {
+    var reservation = req.body;
+
+      reservation.routeName = reservation.name.replace(/\s+/g,"").toLowerCase();
+
+          if(reservation.length <= 5) {
+            tables.push(reservation);
+        } else {
+            waitlist.push(reservation);
+        }
+  
+        res.json(reservation);
+
+      
+  });
+
+
+
+
+
+// Runs server
 app.listen(port, function () {
    console.log("listening on localhost:" + port);
 });
